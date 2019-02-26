1eaaedcdfa1c4ab644124b4a6f678db0fc0d1eaa
Andy Lindsay <andydlindsay@gmail.com>
2019-02-25 19:44:30 -0800

    implements insertNodeAtIndex and removeNodeAtIndex

--- src/LinkedLists/App.java
+++ src/LinkedLists/App.java
@@ -5,17 +5,33 @@ public class App {
 	public static void main(String[] args) {
 
 		LinkedList list = new LinkedList();
-		System.out.println("list length is " + list.getLength());
 		list.printList();
+		System.out.println(list.getLength());
 		
 		list.addNode(new Node(7));
-		System.out.println("list length is " + list.getLength());
 		list.printList();
+		System.out.println(list.getLength());
 		
 		list.addNode(new Node(10));
-		System.out.println("list length is " + list.getLength());
+		list.printList();
+		System.out.println(list.getLength());
+		
+		list.addNode(new Node(3)).addNode(new Node(5));
+		list.printList();
+		System.out.println(list.getLength());
+		
+		list.removeNodeAtIndex(0);
+		list.printList();
+		
+		list.insertNodeAtIndex(new Node(9), 2);
+		list.printList();
+		
+		list.insertNodeAtIndex(new Node(2), 0);
 		list.printList();
 
+		list.insertNodeAtIndex(new Node(11), 17);
+		list.printList();
+		
 	}
 
 }
--- src/LinkedLists/LinkedList.java
+++ src/LinkedLists/LinkedList.java
@@ -11,12 +11,13 @@ public class LinkedList {
 	public int getLength() {
 		int counter = 0;
 		
-		Node currentNode = this.head;
-		if (currentNode != null) {
-			counter += 1;
+		if (this.head == null) {
+			return counter;
 		}
 		
-		while (currentNode != null && currentNode.nextNode != null) {
+		Node currentNode = this.head;
+		
+		while (currentNode != null) {
 			counter += 1;
 			currentNode = currentNode.nextNode;
 		}
@@ -29,16 +30,68 @@ public class LinkedList {
 			System.out.println("linked list is empty");
 			return;
 		}
+		
 		Node currentNode = this.head;
-		System.out.print(currentNode.data + " ");
-		while (currentNode.nextNode != null) {
-			currentNode = currentNode.nextNode;
+		while (currentNode != null) {
 			System.out.print(currentNode.data + " ");
+			currentNode = currentNode.nextNode;
 		}
+		
 		System.out.println();
 	}
 	
-	public void addNode(Node node) {
+	public void insertNodeAtIndex(Node node, int index) {
+		if (index == 0) {
+			node.nextNode = this.head;
+			this.head = node;
+			return;
+		}
+		
+		int counter = 0;
+		Node currentNode = this.head;
+		Node previousNode = null;
+		
+		while (currentNode != null) {
+			if (counter == index) {
+				previousNode.nextNode = node;
+				node.nextNode = currentNode;
+				return;
+			}
+			
+			previousNode = currentNode;
+			currentNode = currentNode.nextNode;
+			counter++;
+		}
+		
+		previousNode.nextNode = node;
+	}
+	
+	public void removeNodeAtIndex(int index) {
+		if (index == 0) {
+			if (this.head.nextNode != null) {
+				this.head = this.head.nextNode;
+			} else {
+				this.head = null;
+			}
+			return;
+		}
+		
+		int counter = 0;
+		Node currentNode = this.head;
+		Node previousNode = null;
+		
+		while (currentNode != null) {
+			if (counter == index) {
+				previousNode.nextNode = currentNode.nextNode;
+				return;
+			}
+			previousNode = currentNode;
+			currentNode = currentNode.nextNode;
+			counter++;
+		}
+	}
+	
+	public LinkedList addNode(Node node) {
 		if (this.head == null) {
 			this.head = node;
 		} else {
@@ -48,6 +101,7 @@ public class LinkedList {
 			}
 			currentNode.nextNode = node;
 		}
+		return this;
 	}
 	
 }
